e5e26b5d7ce050c17e32e30e38f08854cca8dfa1
傲娇的露易丝 <980858415@qq.com>
2017-11-19 15:51:47 +0800

    Initialize

---
+++ UnityEngineLib/UnityEngineLib.cpp
@@ -0,0 +1,8 @@
+#include "UnityEngineLibAPI.h"
+
+static const char* s = "我是李旭";
+
+UNITY_ENGINE_LIB_API const char* Test()
+{
+	return s;
+}
---
+++ UnityEngineLib/UnityEngineLibAPI.h
@@ -0,0 +1,3 @@
+#ifdef _WINDOWS
+#define UNITY_ENGINE_LIB_API extern "C" __declspec(dllexport)
+#endif
